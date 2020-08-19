2edab2b4e943acb7e74ccb5ae666620212ca7c9b
venkai1234 <69787622+venkai1234@users.noreply.github.com>
2020-08-19 13:03:45 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+// import {Post} from './posts/post.model'; //commented if not using event binder and emitters
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //------if using event binders and emitters----
+  // storedPosts:Post[]=[];
+  // onPostAdded(post) {
+  //   this.storedPosts.push(post);
+  // }
+  // -------------
+}
